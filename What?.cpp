b94cdf4ddde8d3d1a4378d9fba4eb7d0ab0ed254
wN0NAMEw <150360386+wN0NAMEw@users.noreply.github.com>
2024-11-28 20:20:01 +0700

    Update What?.cpp
    сделанна игровая карта, может скоро определюсь с конечной целью

--- What?.cpp
+++ What?.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <string>
@@ -7,85 +6,91 @@
 #include <unistd.h>
 #include <vector>
 using namespace std;
-
-class ConAr {
-private:
-  int selectedOption = 0;
-  vector<string> options;
-  int numOptions = options.size();
-
-public:
-  ConAr(vector<string> opt) : options(opt){};
-  int getch() {
-    struct termios oldt, newt;
-    int ch;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return ch;
-  }
-  int gnop() { return numOptions; }
-  vector<string> gvp() { return options; }
-  int gsop() { return selectedOption; }
-  void up() {
-    cout << "\x1B[2J\x1B[H";
-    selectedOption = (selectedOption - 1 + numOptions) % numOptions;
-  }
-  void down() {
-    cout << "\x1B[2J\x1B[H";
-    selectedOption = (selectedOption + 1) % numOptions;
-  }
-  void printOptions() {
-    for (int i = 0; i < numOptions; i++) {
-      if (i == selectedOption) {
-
-        cout << "\033[1;33m> " << options[i] << "\033[0m" << endl;
-      } else {
-        cout << "  " << options[i] << endl;
-      }
-    }
-  }
-};
+// vector<int> ttm;
+// cout << "\x1B[2J\x1B[H";
+string mapGame[10][10] = {
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"},
+{"[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]", "[-]"}};
+int getch() {
+  struct termios oldt, newt;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+}
 
 int main() {
-  ConAr sm({"Option 1",
-            "Option 2",
-            "Option 3",
-            "Exit"});
-  bool teru = true;
-  while (teru) {
-    sm.printOptions();
-    int key = sm.getch();
-    switch (key) {
-    case 99: // c - enter
-      cout << "\x1B[2J\x1B[H";
-      if (sm.gsop() == 0) {
-        cout << "You selected: " << sm.gvp()[0] << endl;
-      } else if (sm.gsop() == 1) {
-        cout << "You selected: " << sm.gvp()[1] << endl;
-      } else if (sm.gsop() == 2) {
-        cout << "You selected: " << sm.gvp()[2] << endl;
-      } else if (sm.gsop() == 3) {
-        cout << "You selected: " << sm.gvp()[3] << endl;
-        teru = false;
-        break;
+
+  int optX = 0;
+  int optY = 0;
+  while (true) {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        mapGame[optX][optY] = "[-]";
+        if((i == optX) and (j == optY)){
+          mapGame[optX][optY] = "[+]";
+          cout <<" \033[1;33m  " << mapGame[i][j] <<"\033[0m";
+        }
+        else{
+          cout << "   " << mapGame[i][j];
+        }
       }
+      cout << endl;
+      cout << endl;
+    }
+
+    int key = getch();
+    switch (key) {
+    case 10:
+      cout << "\x1B[2J\x1B[H";
       break;
 
-    case 122: // z - up
-      sm.up();
+    case 119://key w
+      if (optX > 0) {
+        optX--;
+      }
+      cout << "\x1B[2J\x1B[H";
       break;
 
-    case 120: // x - down
-      sm.down();
+    case 115://key s
+      if (optX < 9) {
+        optX++;
+      }
+      cout << "\x1B[2J\x1B[H";
+      break;
+
+    case 97://key a
+      if (optY > 0) {
+        optY--;
+      }
+      cout << "\x1B[2J\x1B[H";
+      break;
+
+    case 100://key d
+      if (optY < 9) {
+        optY++;
+      }
+      cout << "\x1B[2J\x1B[H";
       break;
 
     default:
       break;
     }
   }
+
   return 0;
 }
+
+
